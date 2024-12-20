5ce991d3a0d4034137ba55589d66a2854566ee90
Aman5685 <amanyadav5685@gmail.com>
2024-12-20 12:58:12 +0530

    V4 Done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,33 @@ import { Toaster } from "react-hot-toast"
 
 //import React from 'react';
 function App() {
+  //code for inspect secure
+  document.addEventListener("contextmenu",function(e){
+    e.preventDefault();
+ });
+ 
+ document.onkeydown=function(e){
+       if(event.keyCode==123)
+       {
+         return false;
+       }
+       if(e.ctrlKey && e.shiftKey && e.keyCode=="I".charCodeAt(0))
+       {
+             return false;
+       }
+       if(e.ctrlKey && e.shiftKey && e.keyCode=="C".charCodeAt(0))
+       {
+          return false;
+       }
+       if(e.ctrlKey && e.shiftKey && e.keyCode=="J".charCodeAt(0))
+       {
+             return false;
+       }
+       if(e.ctrlKey && e.keyCode=="U".charCodeAt(0))
+       { 
+         return false;
+       }
+}
   return (
     <>
     <div>
